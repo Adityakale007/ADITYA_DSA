f6c12981fd7426670a191992030195a5719548b3
ADITYA KALE <adityakale6805@gmail.com>
2025-04-03 23:38:38 +0530

    Time: 0 ms (100%), Space: 13.4 MB (38.35%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 0;i<nums.size();i += 3){
+            if(i == n-1 || nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
