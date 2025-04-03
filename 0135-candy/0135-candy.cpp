b6ac07e2ed6611c4c7f1de8c84a06e52163a5470
ADITYA KALE <adityakale6805@gmail.com>
2025-04-04 00:20:09 +0530

    Time: 0 ms (100%), Space: 22.8 MB (61.4%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> ans(n,1);
+
+        //from left
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                ans[i] = ans[i-1] + 1;
+            }
+        }
+
+        //from right
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1] && ans[i] <= ans[i+1]){
+                ans[i] = ans[i+1] + 1;
+            }
+        }
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=ans[i];
+        }
+        return sum;
+    }
+};
