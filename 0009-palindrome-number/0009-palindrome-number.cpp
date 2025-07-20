ba07b17629ecfed9b3784d686a804a0c7f05bca9
ADITYA KALE <adityakale6805@gmail.com>
2025-07-21 02:11:42 +0530

    Time: 1 ms (60.15%), Space: 8.6 MB (68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original=x;
+        int temp=x;
+        int rev=0;
+        if (temp==0){
+            return true;
+        }
+        while(temp>0){
+            int digit=temp%10;
+            if ((rev > (INT_MAX / 10)) ||
+                (rev < (INT_MIN / 10)))
+            {
+
+                return 0;
+                
+            }
+            rev = rev*10 + digit;
+            temp=temp/10;
+        }
+        if(rev==original){
+            return true;
+        }
+return false;
+    }
+};
