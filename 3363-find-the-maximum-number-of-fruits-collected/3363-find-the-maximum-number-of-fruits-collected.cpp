ee29bd5d828968764344cd01b0f436388383478b
ADITYA KALE <adityakale6805@gmail.com>
2025-08-08 19:46:42 +0530

    Time: 107 ms (47.53%), Space: 174.4 MB (17.9%) - LeetHub

---
+++ 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+
+    int solve2(vector<vector<int>>& fruits,int n,int start_i,int start_j,int end_i,int end_j,vector<vector<int>>& dp){
+
+        //base case
+        if(start_i == end_i && start_j == end_j)
+            return 0;
+        
+
+        //Pruning case
+        if(start_i>end_i)return -1e9;
+        if(start_j>end_j)return -1e9;
+        if(start_i == start_j)return -1e9;
+
+        if(dp[start_i][start_j] != -1)
+            return dp[start_i][start_j];
+
+        int include = 0;
+            int include1 = fruits[start_i][start_j] + solve2(fruits,n,start_i+1,start_j-1,end_i,end_j,dp);
+            int include3 = fruits[start_i][start_j] + solve2(fruits,n,start_i+1,start_j,end_i,end_j,dp);
+            int include2 = fruits[start_i][start_j] + solve2(fruits,n,start_i+1,start_j+1,end_i,end_j,dp);
+
+            include = max(include1,max(include2,include3));
+
+        return dp[start_i][start_j] = include;
+    }
+
+    int solve3(vector<vector<int>>& fruits,int n,int start_i,int start_j,int end_i,int end_j,vector<vector<int>>& dp){
+
+        //base case
+        if(start_i == end_i && start_j == end_j)
+            return 0;
+
+
+        //Pruning case
+        if(start_i>end_i)return -1e9;
+        if(start_j>end_j)return -1e9;
+        if(start_i == start_j)return -1e9;
+
+        if(dp[start_i][start_j] != -1)
+            return dp[start_i][start_j];
+
+        int include = 0;
+            int include1 = fruits[start_i][start_j] + solve3(fruits,n,start_i-1,start_j+1,end_i,end_j,dp);
+            int include3 = fruits[start_i][start_j] + solve3(fruits,n,start_i,start_j+1,end_i,end_j,dp);
+            int include2 = fruits[start_i][start_j] + solve3(fruits,n,start_i+1,start_j+1,end_i,end_j,dp);
+
+            include = max(include1,max(include2,include3));
+
+        return dp[start_i][start_j] = include;
+    }
+
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int n = fruits[0].size();
+
+        int ans1 = 0;
+        for(int i=0;i<n;i++){
+            ans1 += fruits[i][i];
+        }
+
+        vector<vector<int>> dp1(n+1, vector<int> (n+1 , -1));
+        vector<vector<int>> dp2(n+1, vector<int> (n+1 , -1));
+
+        int ans2 = solve2(fruits,n,0,n-1,n-1,n-1,dp1);
+        int ans3 = solve3(fruits,n,n-1,0,n-1,n-1,dp2);
+
+        return ans1+ans2+ans3;
+    }
+};
