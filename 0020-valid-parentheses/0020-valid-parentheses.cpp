57a980abef7c17e7dc21fe8d23d1b7a1f4a64935
ADITYA KALE <adityakale6805@gmail.com>
2025-08-14 20:14:31 +0530

    Time: 0 ms (100%), Space: 10.3 MB (5.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<char, int> symbol = {{'{', -1}, {'[', -2}, {'(', -3},
+                                       {'}', 1},  {']', 2},  {')', 3}};
+    bool isValid(string s) {
+        stack<int> st;
+        for (char bracket : s) {
+            if (symbol[bracket] < 0) {
+                st.push(bracket);
+            } else {
+                if (st.empty())
+                    return false;
+                char top = st.top();
+                st.pop();
+                if ((symbol[top] + symbol[bracket]) != 0) {
+                    return false;
+                }
+            }
+        }
+        if (st.empty())
+            return true;
+        return false;
+    }
+};
