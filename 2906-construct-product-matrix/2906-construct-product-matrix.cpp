41903eaa095db3c6245c4984c9012afe9acf8551
ADITYA KALE <adityakale6805@gmail.com>
2025-10-05 03:40:47 +0530

    Time: 59 ms (35.45%), Space: 158.5 MB (29.11%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int MOD = 12345;
+
+        int n = nums.size();
+        vector<int> v(n,1);
+
+        long long left = 1;
+        for(int i = 0;i<n;i++){
+            v[i] = left;
+            left = (left * nums[i])%MOD;
+        }
+
+        long long right = 1;
+        for(int i = n-1;i>=0;i--){
+            v[i] = (1LL * v[i] * right)%MOD;
+            right = (right * nums[i])%MOD;
+        }
+        return v;
+    }
+
+
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int MOD = 12345;
+
+        vector<int> temp;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                temp.push_back(grid[i][j]);
+            }
+        }
+
+        vector<int> answer = productExceptSelf(temp);
+        vector<vector<int>> ans;
+
+        for(int i=0;i<(n*m);i+=m){
+            vector<int>temp2(answer.begin() + i,answer.begin() + i + m);
+            ans.push_back(temp2);
+        }
+
+        return ans;
+    }
+};
