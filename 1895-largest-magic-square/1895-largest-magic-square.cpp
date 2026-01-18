87f4d289ddab5039fd61c7a93a09a375a3403d64
ADITYA KALE <adityakale6805@gmail.com>
2026-01-18 12:43:08 +0530

    Time: 51 ms (39.53%), Space: 12.9 MB (72.67%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.cpp
+++ 1895-largest-magic-square/1895-largest-magic-square.cpp
@@ -1,60 +1,63 @@
 class Solution {
 public:
-    bool gridCheck(int x,int y,int k,vector<vector<int>>& grid){
+    bool gridCheck(int x, int y, int k, vector<vector<int>>& grid) {
         int targetSum = 0;
 
-        for(int j=0;j<k;j++){
+        for (int j = 0; j < k; j++) {
             targetSum += grid[0 + x][j + y];
         }
 
-        for(int i=0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             int rowCheck = 0;
-            for(int j=0;j<k;j++){
+            for (int j = 0; j < k; j++) {
                 rowCheck += grid[i + x][j + y];
             }
-            if(rowCheck != targetSum)return false;
+            if (rowCheck != targetSum)
+                return false;
         }
-        for(int i=0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             int colCheck = 0;
-            for(int j=0;j<k;j++){
+            for (int j = 0; j < k; j++) {
                 colCheck += grid[j + x][i + y];
             }
-            if(colCheck != targetSum)return false;
+            if (colCheck != targetSum)
+                return false;
         }
 
-        //main diagonal
+        // main diagonal
         int diagonlCheck = 0;
-        for(int i=0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             diagonlCheck += grid[x + i][y + i];
         }
-        if(diagonlCheck != targetSum)return false;
+        if (diagonlCheck != targetSum)
+            return false;
 
-        //anti diagonal
+        // anti diagonal
         diagonlCheck = 0;
-        for(int i=0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             diagonlCheck += grid[x + i][y + k - 1 - i];
         }
-        if(diagonlCheck != targetSum)return false;
+        if (diagonlCheck != targetSum)
+            return false;
 
         return true;
     }
-
 
     int largestMagicSquare(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        int maxi = min(n,m);
+        int maxi = min(n, m);
 
-        for(int k = maxi;k>1;k--){
-            for(int i=0;i+k<=n;i++){
-                for(int j=0;j+k<=m;j++){
-                    if(gridCheck(i,j,k,grid)){
+        for (int k = maxi; k > 1; k--) {
+            for (int i = 0; i + k <= n; i++) {
+                for (int j = 0; j + k <= m; j++) {
+                    if (gridCheck(i, j, k, grid)) {
                         return k;
                     }
                 }
             }
         }
-        return 1;//for 1x1
+        return 1; // for 1x1
     }
 };
