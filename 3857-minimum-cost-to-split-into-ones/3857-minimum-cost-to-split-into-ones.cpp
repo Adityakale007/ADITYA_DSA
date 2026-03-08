d47950d00903700fda25a0be881cfc4d4bbe7f05
ADITYA KALE <adityakale6805@gmail.com>
2026-03-08 21:18:42 +0530

    Time: 134 ms (5.1%), Space: 8.2 MB (29.6%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
+    int dp[501];
+    int rec(int n){
+        if(n==1)return 0;
+
+        if(dp[n] != -1)return dp[n];
+
+        int ans = INT_MAX;
+
+        for(int a = 1;a<n;a++){
+            int b = n-a;
+            ans = min(ans,a*b + rec(a) + rec(b));
+            //current split cost + cost to split a + cost to split b
+        }
+        return dp[n] = ans;
+    }
     int minCost(int n) {
-        return n*(n-1)/2;
-        //as everytime 5 => 4,1 => 3,1,1 => 2,1,1,1 => 1,1,1,1,1
+        memset(dp,-1,sizeof(dp));
+        return rec(n);
     }
 };
