375bc470ffd34c68bcdbac345ac44d5af68ec3b0
ADITYA KALE <adityakale6805@gmail.com>
2026-03-11 10:20:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,27 +11,47 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&v){
-        if(root == NULL)
-            return ;
+    // void inorder(TreeNode* root,vector<int>&v){
+    //     if(root == NULL)
+    //         return ;
         
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
-    }
-    bool isSorted(vector<int>v){
-        int n = v.size();
+    //     inorder(root->left,v);
+    //     v.push_back(root->val);
+    //     inorder(root->right,v);
+    // }
 
-        if(n == 0   ||  n == 1)return true;
+    
+    // bool isSorted(vector<int>v){
+    //     int n = v.size();
 
-        for(int i=0;i<n-1;i++){
-            if(v[i] >= v[i+1])return false;
+    //     if(n == 0   ||  n == 1)return true;
+
+    //     for(int i=0;i<n-1;i++){
+    //         if(v[i] >= v[i+1])return false;
+    //     }
+    //     return true;
+    // }
+
+    bool isBST(TreeNode* root,int min,int max){
+        //base case
+        if(root == NULL)
+            return true;
+        
+        if(root -> val <= min    ||  root -> val >= max){
+            return false;
         }
-        return true;
+
+        return (isBST(root->left,min,root->val)  &&  isBST(root->right,root->val,max));
     }
+    
     bool isValidBST(TreeNode* root) {
-        vector<int>v;
-        inorder(root,v);
-        return isSorted(v);
+        // vector<int>v;
+        //we know in BST inorder traversal is sorted
+        // inorder(root,v);
+        // return isSorted(v);
+
+        // checking with O(1) space
+        //without inorder traversal
+        return isBST(root,INT_MIN,INT_MAX);
     }
 };
