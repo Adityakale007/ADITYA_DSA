c48138daaf16758c3ccf250ca8674e48da53eb9e
ADITYA KALE <adityakale6805@gmail.com>
2026-03-31 19:47:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& arr) {
-        int n = arr.size();
-        stack<int> s;
-        //collosion happens only when
-        //A is going right and B is moving left
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n = a.size();
+        vector<int> st;
+
         for(int i=0;i<n;i++){
-            if(arr[i] > 0){
-                s.push(arr[i]);
-            }
-            else{//for negative element
+            bool destroyed = false;
 
-                //collision happens when
-                while(!s.empty()    &&  s.top()>0   &&  s.top() < abs(arr[i])){
-                    s.pop();
+            while(!st.empty() && st.back()>0 && a[i]<0){
+                if(st.back()    <   -a[i]){
+                    st.pop_back();
                 }
-
-                //equal condition:
-                if(!s.empty()   &&  s.top() == abs(arr[i])){
-                    //separate as ..also can happen when s.top()<0 -> [-8,8]
-                    s.pop();
+                else if(st.back()    ==   -a[i]){
+                    st.pop_back();
+                    destroyed = true;
+                    break;//break as the coming astroid is destroid no more his collisions
                 }
-                else if(s.empty()   ||  s.top()<0){
-                    //negative elements will push only when
-                    s.push(arr[i]);
+                else{
+                    destroyed = true;
+                    break;
                 }
             }
-        }   
 
-
-        vector<int>ans;
-        while(!s.empty()){
-            int curr = s.top();
-            ans.push_back(curr);
-            s.pop();
+            if(!destroyed){
+                st.push_back(a[i]);
+            }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+        return st;
     }
 };
