8b7bbb9964363f3a1c212b71d41c7b52071f5074
ADITYA KALE <adityakale6805@gmail.com>
2026-03-31 21:31:09 +0530

    Time: 8 ms (55.83%), Space: 23 MB (15.78%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,76 +1,76 @@
 class Solution {
-    private:
-        vector<int> nextSmallerElement(vector<int> arr, int n) {
-        stack<int> s;
-        s.push(-1);
-        vector<int> ans(n);
-        for (int i = n - 1; i >= 0; i--) {//we iterated loop from right side
-            int curr = arr[i];
-            while (s.top() != -1 && arr[s.top()] >= curr)
+public:
+    vector<int> nextSmallerElement(vector<int>& h){
+        int n = h.size();
+        stack<int>s;
+        vector<int> ans(n,n);
+
+        for(int i=n-1;i>=0;i--){
+            //in next smaller -> we get an increasing stack
+            while(!s.empty()    &&  h[i] <= h[s.top()] ){
                 s.pop();
-        
-        // ans is stack ka top
-        ans[i] = s.top();
-        s.push(i);
-    }
-        return ans;
-    }
-    vector<int> prevSmallerElement(vector<int> arr, int n) {
-        stack<int> s;
-        s.push(-1);
-        vector<int> ans(n);
-        for (int i = 0; i<n; i++) {
-            int curr = arr[i];
-            while (s.top() != -1 && arr[s.top()] >= curr)
-                s.pop();
-            
-            // ans is stack ka top
-            ans[i] = s.top();
+            }
+            if(!s.empty()){
+                ans[i] = s.top();
+            }
             s.push(i);
         }
         return ans;
-        }
+    }
 
-        int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
+    vector<int> prevSmallerElement(vector<int>& h){
+        int n = h.size();
+        stack<int>s;
+        vector<int> ans(n,-1);
 
-        vector<int> next(n);
-        next = nextSmallerElement(heights, n);
-
-        vector<int> prev(n);
-        prev = prevSmallerElement(heights, n);
-
-        int area = INT_MIN;
-        for (int i = 0; i<n; i++) {
-        int l = heights[i];
-
-            if(next[i] == -1) {
-                next[i] = n;
+        for(int i=0;i<n;i++){
+            //in prev/next smaller -> we get an increasing stack
+            while(!s.empty()    &&  h[i] <= h[s.top()] ){
+                s.pop();
             }
-        int b = next[i] - prev[i] - 1;
-        int newArea = l*b;
-        area = max (area, newArea);
+            if(!s.empty()){
+                ans[i] = s.top();
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+
+
+    int largestRectangleArea(vector<int>& h) {
+        int n = h.size();
+        vector<int> next = nextSmallerElement(h);
+        vector<int> prev = prevSmallerElement(h);
+        int area = 0;
+
+        for(int i=0;i<n;i++){
+            int left = prev[i];
+            int right = next[i];
+            area = max(area , h[i]*(right-left-1));
         }
         return area;
     }
-public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int maxi = INT_MIN;
 
-        vector<int> Histogram(matrix[0].size() , 0);
 
-        for(int i=0; i<matrix.size(); i++){
-            //creating histogram vector
-            for(int j=0; j<Histogram.size(); j++){
-                if(matrix[i][j] == '1'){
-                    Histogram[j]++;
+    int maximalRectangle(vector<vector<char>>& mat) {
+        //same as histogram question
+        int maxArea = 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<int> preSum(m,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == '1'){
+                    preSum[j] += 1;
                 }
                 else{
-                    Histogram[j] = 0;
+                    preSum[j] = 0;
                 }
             }
-            maxi = max(maxi , largestRectangleArea(Histogram));
+
+            maxArea = max(maxArea , largestRectangleArea(preSum));
         }
-        return maxi;
+        return maxArea;
     }
 };
