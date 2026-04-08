ca6b6d2d8a497a7c162a499134ccbc0b85170d39
ADITYA KALE <adityakale6805@gmail.com>
2026-04-08 22:52:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int mod = 1e9 + 7;
-        int n = nums.size();
-        int idx = 0;
-        int ans = 0;
-
-        for(int i=0;i<queries.size();i++){
-            idx = queries[i][0];
-
-            while(idx <= queries[i][1]){
-                nums[idx] = (1LL*nums[idx] * queries[i][3]) % mod;
-                idx += queries[i][2];
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& q) {
+        const int MOD = 1e9 + 7;
+        int n = q.size();
+        for(int i=0;i<n;i++){
+            int index = q[i][0];
+            while(index <= q[i][1]){
+                nums[index] = (nums[index] * q[i][3]) % MOD;
+                index += q[i][2];
             }
         }
-        //XOR of all element after all queries
-        for(int i=0;i<n;i++){
-           ans ^= nums[i];
+
+        int ans = 0;
+        for(int i=0;i<nums.size();i++){
+            ans ^= nums[i];
         }
         return ans;
     }
