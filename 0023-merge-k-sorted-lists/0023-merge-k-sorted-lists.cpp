59745a9c25d6644fd2dee2a389a8b054fa6c2790
ADITYA KALE <adityakale6805@gmail.com>
2026-04-08 23:55:08 +0530

    Time: 0 ms (100%), Space: 18.5 MB (64.72%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    class compare{
+        public:
+        bool operator()(ListNode* a,ListNode* b){
+            return a -> val > b -> val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,compare>minHeap; //as ascending(increasing) order
+
+        //step 1: insert first terms of linked list in minHeap
+        int n = lists.size();
+
+        for(int i=0;i<n;i++){
+            if(lists[i] != NULL){
+                minHeap.push(lists[i]);
+            }
+        }
+
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(minHeap.size() > 0){
+            ListNode* top = minHeap.top();
+            minHeap.pop();
+
+
+
+            if(top->next != NULL){
+                minHeap.push(top->next);
+            }
+
+            if(head == NULL){
+                //if Linked List is empty
+                head = top;
+                tail = top;
+            }
+            else{
+                //insert at tail
+                tail->next = top;
+                tail = top;
+            }
+        }
+        return head;
+    }
+};
