cda45608cdf0dc740ed29a0b630e2e1c18d35bfe
ADITYA KALE <adityakale6805@gmail.com>
2026-04-10 03:43:51 +0530

    Time: 0 ms (100%), Space: 17.9 MB (84.03%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,42 +1,30 @@
-bool sortByValue(const pair<int, int>& a, const pair<int, int>& b) {
-    return a.second > b.second;
-}
 class Solution {
-
 public:
+    class compare{
+        public:
+        bool operator()(pair<int,int>a,pair<int,int>b){
+            return a.second < b.second;
+        }
+    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> m;
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            if (m.count(nums[i])) {
-                m[nums[i]]++;
-            } else {
-                m[nums[i]] = 1;
-            }
+        unordered_map<int,int>mp;
+        for(int n:nums){
+            mp[n]++;
         }
 
-        // multimap<int, int> sortedMap;
-        // for (auto& p : m) {
-        //     sortedMap.insert({p.second, p.first});
-        // }
-        vector<pair<int, int>> v(m.begin(), m.end());
-        sort(v.begin(), v.end(), sortByValue);
-
-        vector<int> ans;
-        for (int i = 0; i < k; i++) {
-            ans.push_back(v[i].first);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,compare>maxHeap;
+        for(auto m:mp){
+            maxHeap.push({m.first,m.second});
         }
-        // sorts on basis of keys
-        // vector<int> v;
-        // for(auto i:sortedMap){
-        //     v.push_back(i.first);
-        // }
-        // vector<int> ans;
-        // for(int i = 0;i<k;i++){
-        //     ans.push_back(v[i]);
-        // }
-
+        vector<int>ans;
+        while(k>0){
+            pair<int,int> temp = maxHeap.top();
+            maxHeap.pop();
+            ans.push_back(temp.first);
+            k--;
+        }
         return ans;
     }
 };
