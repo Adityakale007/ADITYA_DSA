7206fd17089f76a73344a65f79e0ab0787fbe755
ADITYA KALE <adityakale6805@gmail.com>
2026-04-13 22:11:14 +0530

    Time: 7 ms (99.62%), Space: 139.4 MB (25.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // for finding in sliding window
         int n = nums.size();
-        //brute force -> scan to each and every window
-        // int l = 0;
-        // int r = k-1;
-        // vector<int>ans;
-        // while(r<n){
-        //     int maxi = INT_MIN;
-        //     for(int i=l;i<=r;i++){
-        //         maxi = max(maxi,nums[i]);
-        //     }
-        //     ans.push_back(maxi);
-        //     l++;
-        //     r++;
-        // }
-        // return ans;
-        //TC -> O(N2)
+        vector<int>ans;
 
-        
+        //as maximum
+        deque<int>dq; //this is a decreasing deque
 
-        //so here we use double ended queue (dequeue)   ->  DQ
-        //in window -> to insert from one end and to remove from other end
-        //push_front()
-        //pop_front()
-        //push_back()
-        //pop_back()
+        //traverse for first k-sized window
+        for(int i=0;i<k;i++){
+            //addition logic
+            while(!dq.empty()      &&      nums[dq.back()] <= nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        //storing in ans
+        if(!dq.empty()){
+            ans.push_back(nums[dq.front()]);
+        }
 
-        //we will store in monotonic decreasing fashion
-        deque<int> dq;
-        vector<int> ans;
 
-        for(int i=0;i<n;i++){
-            //if window is moved and the previous windows largest element is not present in current window
-            //remove only one element as window moved only by one
-            if(!dq.empty()  &&  dq.front() <= (i-k)){
+        //for remaining k -> n array
+        for(int i=k;i<n;i++){
+            //removal logic
+            if(!dq.empty()      &&      (i - dq.front() >= k)){
                 dq.pop_front();
             }
 
-            // storing in monotonic decreasing fashion
-            while(!dq.empty()   &&  nums[dq.back()] <=  nums[i]){
+            //addition logic
+            while(!dq.empty()      &&      nums[dq.back()] <= nums[i]){
                 dq.pop_back();
             }
+            dq.push_back(i);
 
-            dq.push_back(i);    //store index
-
-            if(i >= k-1){
+            if(!dq.empty()){
                 ans.push_back(nums[dq.front()]);
             }
         }
