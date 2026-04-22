f340a1cf8181704a760b4bda4b22cefe2022c970
ADITYA KALE <adityakale6805@gmail.com>
2026-04-22 14:40:00 +0530

    Time: 8 ms (39.07%), Space: 45.9 MB (19.88%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -22,6 +22,29 @@ public:
         return dp[i][j] = (leftOverflow + rightOverflow);
     }
 
+    double rec_bottom_up(int poured, int query_row, int query_glass){
+        // “How much flows from each glass to the next row?”
+        // We start from the top and propagate overflow downward.
+
+        vector<vector<double>> dp(101, vector<double>(101, 0.0));
+        dp[0][0] = poured;
+
+        for(int i=0;i<query_row;i++){
+            for(int j=0;j<=i;j++){
+
+                if(dp[i][j] > 1.0){
+                    double overflow = (dp[i][j] - 1.0)/2.0;
+
+                    dp[i+1][j] += overflow;
+                    dp[i+1][j+1] += overflow;
+
+                    dp[i][j] = 1.0;// glass stays full
+                }
+            }
+        }
+        return min(1.0 , dp[query_row][query_glass]);
+    }
+
     double champagneTower(int poured, int query_row, int query_glass) {
         //my intuition is dp cause recursion is used
         // Why Dynamic Programming?
@@ -29,13 +52,16 @@ public:
         //         Every glass depends only on the previous row.
         //         The tower height is small (≤ 100).
 
-         for (int i = 0; i < 101; i++){
-            for (int j = 0; j < 101; j++){
-                dp[i][j] = -1.0;
-            }
-         }
+
+
+        //  for (int i = 0; i < 101; i++){
+        //     for (int j = 0; j < 101; j++){
+        //         dp[i][j] = -1.0;
+        //     }
+        //  }
                 
-        double ans = rec(poured,query_row,query_glass);
-        return min(1.0 , ans);
+        // double ans = rec(poured,query_row,query_glass);
+        // return min(1.0 , ans);
+        return rec_bottom_up(poured,query_row,query_glass);
     }
 };
