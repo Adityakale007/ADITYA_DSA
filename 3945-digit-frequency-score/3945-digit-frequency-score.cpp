9775dfdc171dd481908c953f498afb6272bf09c7
ADITYA KALE <adityakale6805@gmail.com>
2026-05-31 15:56:39 +0530

    Time: 2 ms (23.25%), Space: 9.2 MB (40.96%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        unordered_map<int,int>mp;
+        while(n>0){
+            mp[n%10]++;
+            n /= 10;
+        }
+        int ans = 0;
+        for(auto m:mp){
+            ans += (m.first * m.second);
+        }
+        return ans;
+    }
+};
